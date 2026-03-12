c911d394cbd143ac49cb4ba926074603d354e316
zinshin36 <leewd52@gmail.com>
2026-03-12 19:13:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import tkinter as tk
+import logging
+
 from gui.app import App
+from engine.logger import setup_logger
+from engine.runtime_paths import BASE_DIR
 
 
 def main():
+
+    setup_logger()
+
+    logging.info(f"Running from: {BASE_DIR}")
 
     root = tk.Tk()
 
