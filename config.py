c5840b5d447a52e3203a7d051caded746d713bb5
zinshin36 <leewd52@gmail.com>
2026-03-12 18:56:46 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+MAX_RESULTS = 10
+
+ILVL_WINDOW = 25
+
+GAME_DATA_FOLDER = "game_data"
+
+STAT_WEIGHTS = {
+    "WeaponDamage": 10.0,
+    "Intelligence": 1.0,
+    "CriticalHit": 0.22,
+    "Determination": 0.18,
+    "DirectHit": 0.17,
+    "SpellSpeed": 0.12
+}
+
+BLACKLIST = []
