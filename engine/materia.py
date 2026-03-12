5d1bf379cc00500f01c24e5d24e5494254bd9012
zinshin36 <leewd52@gmail.com>
2026-03-12 19:20:20 -0400

    Create materia.py

---
+++ engine/materia.py
@@ -0,0 +1,15 @@
+def apply_materia(item, materia_list):
+    """Apply materia to the item stats."""
+    slots = item.get("materia_slots", 0)
+    best_stats = {}
+
+    # pick best materia for simplicity
+    sorted_materia = sorted(materia_list, key=lambda x: list(x["stats"].values())[0], reverse=True)
+
+    for i in range(slots):
+        if i < len(sorted_materia):
+            stat, val = list(sorted_materia[i]["stats"].items())[0]
+            item["stats"][stat] = item["stats"].get(stat, 0) + val
+
+    item["MateriaApplied"] = slots
+    return item
