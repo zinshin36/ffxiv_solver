60a903a48745dfdb1b16ecc05ffe9c8c4d3bc8c9
zinshin36 <leewd52@gmail.com>
2026-03-12 19:12:32 -0400

    Create logger.py

---
+++ engine/logger.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from datetime import datetime
+from engine.runtime_paths import LOG_DIR
+
+
+def setup_logger():
+
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    logfile = os.path.join(LOG_DIR, f"run_{timestamp}.log")
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(logfile, encoding="utf-8"),
+            logging.StreamHandler()
+        ]
+    )
+
+    logging.info("===================================")
+    logging.info("Application Started")
+    logging.info(f"Log file: {logfile}")
+
+    return logfile
