c3a0676f07b0590fc42511ce3e700593f82dc9dd
zinshin36 <leewd52@gmail.com>
2026-03-12 19:21:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import tkinter as tk
-import logging
-
 from gui.app import App
 from engine.logger import setup_logger
-from engine.runtime_paths import BASE_DIR
-
 
 def main():
-
     setup_logger()
-
-    logging.info(f"Running from: {BASE_DIR}")
-
     root = tk.Tk()
-
     App(root)
-
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
