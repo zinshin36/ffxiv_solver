4e67f96ebfe43850fd90c91e059fe568dcfc179d
zinshin36 <leewd52@gmail.com>
2026-03-13 21:30:33 -0400

    Update logger.py

--- engine/logger.py
+++ engine/logger.py
@@ -4,10 +4,16 @@ from datetime import datetime
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 
-log_file = os.path.join(
+main_log_file = os.path.join(
     LOG_DIR,
     f"log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
 )
+
+csv_log_file = os.path.join(
+    LOG_DIR,
+    f"csv_debug_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+)
+
 
 def log(message):
 
@@ -17,5 +23,15 @@ def log(message):
 
     print(line)
 
-    with open(log_file, "a", encoding="utf-8") as f:
+    with open(main_log_file, "a", encoding="utf-8") as f:
+        f.write(line + "\n")
+
+
+def csv_log(message):
+
+    ts = datetime.now().strftime("%H:%M:%S")
+
+    line = f"[{ts}] {message}"
+
+    with open(csv_log_file, "a", encoding="utf-8") as f:
         f.write(line + "\n")
