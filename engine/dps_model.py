b2c0f9d5e58c8134c34ec5707e301338d3842dc3
zinshin36 <leewd52@gmail.com>
2026-03-15 21:55:44 -0400

    Update dps_model.py

--- engine/dps_model.py
+++ engine/dps_model.py
@@ -1,23 +1,17 @@
-def expected_dps(stats):
+def compute_dps(stats):
 
-    main = stats.get("Intelligence", 0)
-    crit = stats.get("CriticalHit", 0)
-    det = stats.get("Determination", 0)
-    dh = stats.get("DirectHitRate", 0)
-    sps = stats.get("SpellSpeed", 0)
+    int_stat = stats.get("int", 0)
+    crit = stats.get("crit", 0)
+    det = stats.get("det", 0)
+    dh = stats.get("dh", 0)
+    sps = stats.get("sps", 0)
 
-    crit_rate = 0.05 + crit / 5000
-    dh_rate = dh / 5500
-
-    crit_bonus = 1.6
-
-    det_multi = 1 + det / 10000
-    speed_multi = 1 + sps / 20000
-
-    return (
-        main *
-        (1 + crit_rate * (crit_bonus - 1)) *
-        (1 + dh_rate * 0.25) *
-        det_multi *
-        speed_multi
+    dps = (
+        int_stat * 1.0 +
+        crit * 0.45 +
+        det * 0.30 +
+        dh * 0.35 +
+        sps * 0.18
     )
+
+    return dps
