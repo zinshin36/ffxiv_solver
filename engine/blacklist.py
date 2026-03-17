42e0030b9477688704f3bd7dcc70dcd854fa310e
zinshin36 <leewd52@gmail.com>
2026-03-16 23:13:42 -0400

    Update blacklist.py

--- engine/blacklist.py
+++ engine/blacklist.py
@@ -1,10 +1,20 @@
-def is_blacklisted(item, blacklist):
+import os
 
-    name = item["name"].lower()
+def load_blacklist():
 
-    for b in blacklist:
+    if not os.path.exists("blacklist.txt"):
+        return []
 
-        if b.lower() in name:
+    with open("blacklist.txt","r",encoding="utf8") as f:
+        return [x.strip().lower() for x in f.readlines() if x.strip()]
+
+
+def is_blacklisted(name, blacklist):
+
+    name = name.lower()
+
+    for word in blacklist:
+        if word in name:
             return True
 
     return False
