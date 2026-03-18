6fceb3401ed633814f3614bee707788bc55cec59
zinshin36 <leewd52@gmail.com>
2026-03-18 07:04:19 -0400

    Update dps_model.py

--- engine/dps_model.py
+++ engine/dps_model.py
@@ -5,10 +5,19 @@ def compute_dps(stats):
     sps = stats["sps"]
     intel = stats.get("int", 1000)
 
-    # simplified scaling but WORKING
-    crit_multi = 1 + (crit / 2000)
-    dh_multi = 1 + (dh / 2500)
-    det_multi = 1 + (det / 3000)
-    sps_multi = 1 + (sps / 4000)
+    # scaling
+    crit_multi = 1 + (crit / 1900)
+    dh_multi = 1 + (dh / 2300)
+    det_multi = 1 + (det / 2700)
+
+    # SPELL SPEED BREAKPOINT VALUE
+    if sps >= 1400:
+        sps_multi = 1.12
+    elif sps >= 1200:
+        sps_multi = 1.09
+    elif sps >= 1000:
+        sps_multi = 1.06
+    else:
+        sps_multi = 1.02
 
     return intel * crit_multi * dh_multi * det_multi * sps_multi
