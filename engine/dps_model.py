0a9ec014b85703aa753050bd25c50acbe891947e
zinshin36 <leewd52@gmail.com>
2026-03-18 06:58:04 -0400

    Update dps_model.py

--- engine/dps_model.py
+++ engine/dps_model.py
@@ -1,41 +1,14 @@
-def crit_multiplier(crit):
-
-    rate = (crit - 400) / 1900 + 0.05
-    dmg = (crit - 400) / 1900 + 1.4
-
-    return 1 + rate * (dmg - 1)
-
-
-def dh_multiplier(dh):
-
-    rate = (dh - 400) / 1900
-
-    return 1 + rate * 0.25
-
-
-def det_multiplier(det):
-
-    return 1 + (det - 390) / 1900
-
-
-def sps_multiplier(sps):
-
-    return 1 + (sps - 400) / 2000
-
-
 def compute_dps(stats):
+    crit = stats["crit"]
+    dh = stats["dh"]
+    det = stats["det"]
+    sps = stats["sps"]
+    intel = stats.get("int", 1000)
 
-    INT = stats.get("int", 0)
+    # simplified scaling but WORKING
+    crit_multi = 1 + (crit / 2000)
+    dh_multi = 1 + (dh / 2500)
+    det_multi = 1 + (det / 3000)
+    sps_multi = 1 + (sps / 4000)
 
-    crit = stats.get("crit", 0)
-    dh = stats.get("dh", 0)
-    det = stats.get("det", 0)
-    sps = stats.get("sps", 0)
-
-    return (
-        INT *
-        crit_multiplier(crit) *
-        dh_multiplier(dh) *
-        det_multiplier(det) *
-        sps_multiplier(sps)
-    )
+    return intel * crit_multi * dh_multi * det_multi * sps_multi
