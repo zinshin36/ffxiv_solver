953cac108c9bde7f74537d274a61cfa6d06124aa
zinshin36 <leewd52@gmail.com>
2026-03-19 15:19:35 -0400

    Update gear_filter.py

--- engine/gear_filter.py
+++ engine/gear_filter.py
@@ -1,3 +1,5 @@
+# REPLACE ENTIRE FILE
+
 from config import ILVL_WINDOW
 from engine.blacklist import is_blacklisted
 
@@ -5,7 +7,6 @@ from engine.blacklist import is_blacklisted
 def filter_items(items, blacklist):
 
     max_ilvl = max(x["ilvl"] for x in items)
-
     cutoff = max_ilvl - ILVL_WINDOW
 
     out = []
@@ -15,25 +16,9 @@ def filter_items(items, blacklist):
         if item["ilvl"] < cutoff:
             continue
 
-        if is_blacklisted(item, blacklist):
+        if is_blacklisted(item["name"], blacklist):
             continue
 
         out.append(item)
 
     return out
-
-
-def group_by_slot(items):
-
-    slots = {}
-
-    for i in items:
-
-        slot = i["slot"]
-
-        if slot not in slots:
-            slots[slot] = []
-
-        slots[slot].append(i)
-
-    return slots
