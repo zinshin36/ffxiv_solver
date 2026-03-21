1d32bbec3be6f1b651cc022064b2c763f856c5de
zinshin36 <leewd52@gmail.com>
2026-03-21 11:13:33 -0400

    Update optimizer.py

--- engine/optimizer.py
+++ engine/optimizer.py
@@ -1,6 +1,6 @@
 import itertools
-from engine.melds import generate_melded_stats
 from engine.stats import calculate_build_stats, cap_stats
+from engine.melds import generate_melded_stats
 
 def run_solver(items_by_slot, min_ilvl=0, target_gcd=None, build_type="Crit", selected_food=None, blacklist=None):
     all_slots = ["weapon","head","body","hands","legs","feet","earrings","necklace","bracelet","ring1","ring2"]
@@ -11,32 +11,30 @@ def run_solver(items_by_slot, min_ilvl=0, target_gcd=None, build_type="Crit", se
     for items in slot_items:
         total_combinations *= len(items)
 
-    # limit combinations if too large
-    if total_combinations > 10**7:
+    if total_combinations > 10**7:  # cap for performance
         slot_items = [items[:10] for items in slot_items]
 
     for combo in itertools.product(*slot_items):
         build = {slot: item.copy() for slot, item in zip(all_slots, combo)}
-        # generate melds
+
+        # generate melds per item
         for slot_name, item in build.items():
             item['melds'] = generate_melded_stats(item, build_type)
 
         stats = calculate_build_stats(build, selected_food)
         stats = cap_stats(stats)
-        dps = stats['dps']
-        gcd = stats['gcd']
 
         build_entry = {
             'items': build,
-            'dps': dps,
-            'gcd': gcd,
+            'dps': stats['dps'],
+            'gcd': stats['gcd'],
             'crit': stats['crit'],
             'dh': stats['dh'],
             'det': stats['det'],
             'sps': stats['sps']
         }
+
         best_builds.append(build_entry)
 
-    # sort by dps
     best_builds.sort(key=lambda x: x['dps'], reverse=True)
-    return best_builds[:3]  # top 3
+    return best_builds[:3]
